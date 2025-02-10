c97fbf8775c35c41716caa2f4bb9d0f8ccbfdd6e
Rasel Miah <rasel6041@gmail.com>
2025-02-10 18:40:07 +0100

    create product

--- app/dashboard/products/create/page.tsx
+++ app/dashboard/products/create/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-/* import { createProduct } from "@/app/actions"; */
+import { createProduct } from "@/app/actions";
 import { UploadDropzone } from "@/app/lib/uplaodthing";
 import { Button } from "@/components/ui/button";
 import {
@@ -24,19 +24,18 @@ import { Switch } from "@/components/ui/switch";
 import { Textarea } from "@/components/ui/textarea";
 import { ChevronLeft, XIcon } from "lucide-react";
 import Link from "next/link";
-import { useFormState } from "react-dom";
-/* import { useForm } from "@conform-to/react";
+import { useForm } from "@conform-to/react"; 
 import { parseWithZod } from "@conform-to/zod";
-import { productSchema } from "@/app/lib/zodSchemas"; */
-import { useState } from "react";
+import { productSchema } from "@/app/lib/zodSchemas";
+import { useActionState, useState } from "react";
 
 import Image from "next/image";
 import { SubmitButton } from "@/app/components/SubmitButtons";
-/* import { categories } from "@/app/lib/categories"; */
+import { categories } from "@/app/lib/categories";
 
 export default function ProductCreateRoute() {
- /*  const [images, setImages] = useState<string[]>([]);
-  const [lastResult, action] = useFormState(createProduct, undefined);
+ const [images, setImages] = useState<string[]>([]);
+  const [lastResult, action] = useActionState(createProduct, undefined);
   const [form, fields] = useForm({
     lastResult,
 
@@ -50,10 +49,10 @@ export default function ProductCreateRoute() {
 
   const handleDelete = (index: number) => {
     setImages(images.filter((_, i) => i !== index));
-  }; */
+  };
 
   return (
-    <form /* id={form.id} onSubmit={form.onSubmit} action={action} */>
+    <form id={form.id} onSubmit={form.onSubmit} action={action}>
       <div className="flex items-center gap-4">
         <Button variant="outline" size="icon" asChild>
           <Link href="/dashboard/products">
@@ -76,54 +75,54 @@ export default function ProductCreateRoute() {
               <Label>Name</Label>
               <Input
                 type="text"
-               /*  key={fields.name.key}
+                key={fields.name.key}
                 name={fields.name.name}
-                defaultValue={fields.name.initialValue} */
+                defaultValue={fields.name.initialValue}
                 className="w-full"
                 placeholder="Product Name"
               />
 
-              {/* <p className="text-red-500">{fields.name.errors}</p> */}
+              <p className="text-red-500">{fields.name.errors}</p>
             </div>
 
             <div className="flex flex-col gap-3">
               <Label>Description</Label>
               <Textarea
-                /* key={fields.description.key}
+                key={fields.description.key}
                 name={fields.description.name}
-                defaultValue={fields.description.initialValue} */
+                defaultValue={fields.description.initialValue}
                 placeholder="Write your description right here..."
               />
-              {/* <p className="text-red-500">{fields.description.errors}</p> */}
+              <p className="text-red-500">{fields.description.errors}</p>
             </div>
             <div className="flex flex-col gap-3">
               <Label>Price</Label>
               <Input
-                /* key={fields.price.key}
+                key={fields.price.key}
                 name={fields.price.name}
-                defaultValue={fields.price.initialValue} */
+                defaultValue={fields.price.initialValue}
                 type="number"
                 placeholder="$55"
               />
-             {/*  <p className="text-red-500">{fields.price.errors}</p> */}
+              <p className="text-red-500">{fields.price.errors}</p>
             </div>
 
             <div className="flex flex-col gap-3">
               <Label>Featured Product</Label>
               <Switch
-                /* key={fields.isFeatured.key}
+                key={fields.isFeatured.key}
                 name={fields.isFeatured.name}
-                defaultValue={fields.isFeatured.initialValue} */
+                defaultValue={fields.isFeatured.initialValue}
               />
-              {/* <p className="text-red-500">{fields.isFeatured.errors}</p> */}
+              <p className="text-red-500">{fields.isFeatured.errors}</p>
             </div>
 
             <div className="flex flex-col gap-3">
               <Label>Status</Label>
               <Select
-               /*  key={fields.status.key}
+                key={fields.status.key}
                 name={fields.status.name}
-                defaultValue={fields.status.initialValue} */
+                defaultValue={fields.status.initialValue}
               >
                 <SelectTrigger>
                   <SelectValue placeholder="Select Status" />
@@ -134,40 +133,40 @@ export default function ProductCreateRoute() {
                   <SelectItem value="archived">Archived</SelectItem>
                 </SelectContent>
               </Select>
-              {/* <p className="text-red-500">{fields.status.errors}</p> */}
+              <p className="text-red-500">{fields.status.errors}</p>
             </div>
 
             <div className="flex flex-col gap-3">
               <Label>Category</Label>
               <Select
-                /* key={fields.category.key}
+                key={fields.category.key}
                 name={fields.category.name}
-                defaultValue={fields.category.initialValue} */
+                defaultValue={fields.category.initialValue}
               >
                 <SelectTrigger>
                   <SelectValue placeholder="Select Category" />
                 </SelectTrigger>
                 <SelectContent>
-                 {/*  {categories.map((category) => (
+                  {categories.map((category) => (
                     <SelectItem key={category.id} value={category.name}>
                       {category.title}
                     </SelectItem>
-                  ))} */}
+                  ))}
                 </SelectContent>
               </Select>
-              {/* <p className="text-red-500">{fields.category.errors}</p> */}
+              <p className="text-red-500">{fields.category.errors}</p>
             </div>
 
             <div className="flex flex-col gap-3">
               <Label>Images</Label>
               <input
                 type="hidden"
-                /* value={images}
+                value={images}
                 key={fields.images.key}
                 name={fields.images.name}
-                defaultValue={fields.images.initialValue as any} */
+                defaultValue={fields.images.initialValue as any}
               />
-            {/*   {images.length > 0 ? (
+              {images.length > 0 ? (
                 <div className="flex gap-5">
                   {images.map((image, index) => (
                     <div key={index} className="relative w-[100px] h-[100px]">
@@ -189,20 +188,20 @@ export default function ProductCreateRoute() {
                     </div>
                   ))}
                 </div>
-              ) : ( */}
+              ) : (
                 <UploadDropzone
                   endpoint="imageUploader"
                   onClientUploadComplete={(res) => {
-                    /* setImages(res.map((r) => r.url)); */
+                    setImages(res.map((r) => r.url));
                     alert("Finish uploading");
                   }}
                   onUploadError={() => {
                     alert("Something went wrong");
                   }}
                 />
-             {/*  )} */}
+              )}
 
-             {/*  <p className="text-red-500">{fields.images.errors}</p> */}
+              <p className="text-red-500">{fields.images.errors}</p>
             </div>
           </div>
         </CardContent>
