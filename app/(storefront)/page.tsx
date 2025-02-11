92f422cde65ffa12fbbd5c95cd019d05b3f6f313
Rasel Miah <rasel6041@gmail.com>
2025-02-11 19:41:57 +0100

    add banner on hero

--- app/(storefront)/page.tsx
+++ app/(storefront)/page.tsx
@@ -1,12 +1,12 @@
 /* import { CategoriesSelection } from "../components/storefront/CategorySelection";
-import { FeaturedProducts } from "../components/storefront/FeaturedProducts";
-import { Hero } from "../components/storefront/Hero"; */
+import { FeaturedProducts } from "../components/storefront/FeaturedProducts"; */
+import { Hero } from "../components/storefront/Hero";
 import { Navbar } from "../components/storefront/Navbar";
 
 export default function IndexPage() {
   return (
     <div>
-      {/* <Hero />
+      <Hero />{/* 
       <CategoriesSelection />
       <FeaturedProducts /> */}
     </div>
