d30fc193381bbc564dd5148f11db518544c983e0
Rasel Miah <rasel6041@gmail.com>
2025-02-12 19:38:29 +0100

    order page

--- app/dashboard/orders/page.tsx
+++ app/dashboard/orders/page.tsx
@@ -1,4 +1,4 @@
-/* import prisma from "@/app/lib/db"; */
+import prisma from "@/app/lib/db";
 import {
   Card,
   CardContent,
@@ -16,7 +16,7 @@ import {
 } from "@/components/ui/table";
 import { unstable_noStore as noStore } from "next/cache";
 
-/* async function getData() {
+async function getData() {
   const data = await prisma.order.findMany({
     select: {
       amount: true,
@@ -37,11 +37,11 @@ import { unstable_noStore as noStore } from "next/cache";
   });
 
   return data;
-} */
+}
 
 export default async function OrdersPage() {
   noStore();
- /*  const data = await getData(); */
+  const data = await getData();
   return (
     <Card>
       <CardHeader className="px-7">
@@ -60,24 +60,24 @@ export default async function OrdersPage() {
             </TableRow>
           </TableHeader>
           <TableBody>
-            {/* {data.map((item) => ( */}
-              <TableRow /* key={item.id} */>
+            {data.map((item) => (
+              <TableRow key={item.id}>
                 <TableCell>
-                  <p className="font-medium">{/* {item.User?.firstName} */}name</p>
+                  <p className="font-medium">{item.User?.firstName}</p>
                   <p className="hidden md:flex text-sm text-muted-foreground">
-                    {/* {item.User?.email} */}email
+                    {item.User?.email}
                   </p>
                 </TableCell>
                 <TableCell>Order</TableCell>
-                <TableCell>{/* {item.status} */}sttus</TableCell>
+                <TableCell>{item.status}</TableCell>
                 <TableCell>
-                  {/* {new Intl.DateTimeFormat("en-US").format(item.createdAt)} */}time
+                  {new Intl.DateTimeFormat("en-US").format(item.createdAt)}
                 </TableCell>
                 <TableCell className="text-right">
-                  ${/* {new Intl.NumberFormat("en-US").format(item.amount / 100)} */}100
+                  ${new Intl.NumberFormat("en-US").format(item.amount / 100)}
                 </TableCell>
               </TableRow>
-           {/*  ))} */}
+           ))}
           </TableBody>
         </Table>
       </CardContent>
