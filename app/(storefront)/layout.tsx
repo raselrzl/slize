211c1dc4b4737cbba38d68569a4ab0c372f47022
Rasel Miah <rasel6041@gmail.com>
2025-02-12 11:00:06 +0100

    footer

--- app/(storefront)/layout.tsx
+++ app/(storefront)/layout.tsx
@@ -1,6 +1,6 @@
 import { type ReactNode } from "react";
 import { Navbar } from "../components/storefront/Navbar";
-/* import { Footer } from "../components/storefront/Footer"; */
+import { Footer } from "../components/storefront/Footer";
 
 export default function StoreFrontLayout({
   children,
@@ -11,7 +11,7 @@ export default function StoreFrontLayout({
     <>
       <Navbar />
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">{children}</main>
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
