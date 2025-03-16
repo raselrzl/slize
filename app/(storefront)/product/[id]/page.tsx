31366427757fa245a86659a6aa090180fed6c105
Rasel Miah <rasel6041@gmail.com>
2025-03-16 20:24:18 +0100

    added items button if the user is not logi

--- app/(storefront)/product/[id]/page.tsx
+++ app/(storefront)/product/[id]/page.tsx
@@ -3,10 +3,17 @@ import { ShoppingBagButton } from "@/app/components/SubmitButtons";
 import { FeaturedProducts } from "@/app/components/storefront/FeaturedProducts";
 import { ImageSlider } from "@/app/components/storefront/ImageSlider";
 import prisma from "@/app/lib/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+import {
+  LoginLink,
+  RegisterLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
 
 import { StarIcon } from "lucide-react";
 import { notFound } from "next/navigation";
 import { unstable_noStore as noStore } from "next/cache";
+import { Button } from "@/components/ui/button";
 
 async function getData(productId: string) {
   const data = await prisma.product.findUnique({
@@ -38,6 +45,8 @@ export default async function ProductIdRoute({
   const params = await paramsPromise;
   const data = await getData(params.id);
   const addProducttoShoppingCart = addItem.bind(null, data.id);
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
   return (
     <>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 items-start lg:gap-x-24 py-6">
@@ -55,10 +64,27 @@ export default async function ProductIdRoute({
             <StarIcon className="h-4 w-4 text-yellow-500 fill-yellow-500" />
           </div>
           <p className="text-base text-gray-700 mt-6">{data.description}</p>
+          {user ? (
+            <>
+              {/* If user is logged in, show the form with the button */}
+              <form action={addProducttoShoppingCart}>
+                <ShoppingBagButton />
+              </form>
+            </>
+          ) : (
+            <>
+              {/* If no user, show the LoginLink*/}
+              <div className="w-full mt-5">
+                <Button variant="destructive" size="lg" className="w-full">
+                  <LoginLink>Login to Add items to Cart</LoginLink>
+                </Button>
+              </div>
+            </>
+          )}
 
-          <form action={addProducttoShoppingCart}>
+          {/* <form action={addProducttoShoppingCart}>
             <ShoppingBagButton />
-          </form>
+          </form> */}
         </div>
       </div>
 
