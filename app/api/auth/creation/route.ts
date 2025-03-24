9bc2f45dacf36ec1bf1c38e68c41245149cf6147
Rasel Miah <rasel6041@gmail.com>
2025-03-24 20:58:52 +0100

    added items button if the user is not logi

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -34,6 +34,6 @@ export async function GET() {
   return NextResponse.redirect(
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000/"
-      : "https://slize-rust.vercel.app/"
+      : "https://kronstil.store/"
   );
 }
