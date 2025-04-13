dda33f676a5e357832c7284741106b600ab71356
Rasel Miah <rasel6041@gmail.com>
2025-04-13 17:28:47 +0100

    not found page

--- app/components/storefront/NewArrival.tsx
+++ app/components/storefront/NewArrival.tsx
@@ -1,5 +1,11 @@
 // components/NewArrival.tsx
-import { ArrowBigRightDash, FileQuestionIcon, Info, InfoIcon, MessageCircleQuestion } from "lucide-react";
+import {
+  ArrowBigRightDash,
+  FileQuestionIcon,
+  Info,
+  InfoIcon,
+  MessageCircleQuestion,
+} from "lucide-react";
 import Link from "next/link";
 import React from "react";
 import InfoAlert from "./infoAlert";
@@ -10,7 +16,6 @@ const NewArrival = () => {
       <div className="flex flex-col md:flex-row w-full max-w-6xl gap-4">
         <div className="flex-1 w-full h-[250px] my-2 flex items-center justify-center">
           <div>
-            
             <InfoAlert />
             <p className="text-xl md:text-2xl font-extrabold text-white">
               Keep eyes on new arrival
@@ -25,12 +30,28 @@ const NewArrival = () => {
             </div>
           </div>
         </div>
-        <div className="flex-1 w-full h-[250px] my-2 bg-white rounded-md shadow-md overflow-hidden">
-          <img
-            src="/homepage.png"
-            alt="Homepage"
-            className="w-full h-full object-contain"
-          />
+        <div className="flex-1 w-full h-[250px] my-2 bg-red-800 overflow-hidden flex gap-1 px-1">
+          <div className="w-1/3 h-full flex items-center justify-center">
+            <img
+              src="/newaraival/n1.jpg"
+              alt="n1"
+              className="w-full h-[90%] object-cover shadow opacity-2 border-8 border-gray-200"
+            />
+          </div>
+          <div className="w-1/3 h-full flex items-end justify-center">
+            <img
+              src="/newaraival/n3.jpg"
+              alt="n3"
+              className="w-full h-[80%] object-cover shadow opacity-1 border-8 border-gray-200"
+            />
+          </div>
+          <div className="w-1/3 h-full flex items-start justify-center">
+            <img
+              src="/newaraival/n2.jpg"
+              alt="n2"
+              className="w-full h-[85%] object-cover shadow border-8 border-gray-200"
+            />
+          </div>
         </div>
       </div>
     </div>
