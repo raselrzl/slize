0a23fc973412171ad4e75cefbd2580cf0fef8ea3
Rasel Miah <rasel6041@gmail.com>
2025-04-13 13:46:12 +0100

    not found page

--- app/components/storefront/NewArrival.tsx
+++ app/components/storefront/NewArrival.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const NewArrival = () => {
   return (
-    <div className="flex flex-wrap gap-4 bg-red-800 text-white w-full max-w-screen-xl mx-auto px-4 py-8">
+    <div className="flex flex-wrap gap-4 bg-red-800 text-white w-full max-w-screen-xl mx-auto px-4 py-8 mt-2">
       <div className="flex flex-col md:flex-row w-full max-w-6xl gap-4">
         <div className="flex-1 w-full h-[250px] my-2 flex items-center justify-center">
           <p className="text-xl md:text-2xl font-extrabold border-2 p-4 text-center text-white">
