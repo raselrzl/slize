e9b75228fba9710d335acf5f8cf934d793d1be5b
raselrzl <rasel6041@gmail.com>
2025-11-04 11:56:36 +0100

    added 200px

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -30,7 +30,7 @@ export default async function DashboardLayout({
   }
   return (
     <div className="flex w-full flex-col max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-10">
-      <header className="sticky top-0 flex h-16 items-center justify-between gap-4 border border-gray-400 bg-gray-700 text-white">
+      <header className="sticky top-0 flex h-16 items-center justify-between gap-4 border border-gray-700 bg-gray-700 text-white">
         <nav className="hidden font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6 ml-6">
           <DashboardNavigation />
         </nav>
