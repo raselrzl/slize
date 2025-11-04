2a93121cee4d178cd9ba9178bd46c355e9b3f100
raselrzl <rasel6041@gmail.com>
2025-11-04 23:05:56 +0100

    pagination on order page

--- app/components/category/AllkidGirlsfourtofiveProductBanner.tsx
+++ app/components/category/AllkidGirlsfourtofiveProductBanner.tsx
@@ -6,7 +6,7 @@ export function AllkidGirlsfourtofiveProductBanner() {
     <section className="bg-gray-900 text-white py-4 px-2 md:px-0">
       <div className="flex flex-col md:flex-row items-center justify-between md:items-stretch gap-6 max-w-7xl mx-auto">
                {/* Right side text section */}
-        <div className="flex flex-col justify-center items-start gap-4 md:gap-6 md:w-1/2 text-left">
+        <div className="flex flex-col px-2 justify-center items-start gap-4 md:gap-6 md:w-1/2 text-left">
           <div className="flex justify-center md:justify-start">
             <FileQuestion className="w-10 h-10 text-yellow-300"/>
           </div>
