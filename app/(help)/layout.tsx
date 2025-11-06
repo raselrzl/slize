f12e159d62e02b966b7c58d49dd76145ddbda98b
raselrzl <rasel6041@gmail.com>
2025-11-06 11:10:31 +0100

    card session design

--- app/(help)/layout.tsx
+++ app/(help)/layout.tsx
@@ -1,6 +1,8 @@
 import { ReactNode } from "react";
 import { HelpSidebar } from "./HelpSidebar";
 import { Footer } from "../components/storefront/Footer";
+import Link from "next/link";
+import Image from "next/image";
 
 interface HelpLayoutProps {
   children: ReactNode;
@@ -9,6 +11,17 @@ interface HelpLayoutProps {
 export default function HelpLayout({ children }: HelpLayoutProps) {
   return (
     <div className="bg-gray-200 min-h-screen max-w-5xl mx-auto">
+              <div className="flex justify-center items-center bg-black">
+          <Link href="/">
+            <Image
+              src="/logo/kw.png"
+              alt="Kron Word Logo"
+              width={200}
+              height={160}
+              className="h-[35px] w-[140px]"
+            />
+          </Link>
+        </div>
       {/* Wrapper for sidebar + content */}
       <div className="max-w-7xl mx-auto flex flex-col md:flex-row">
         {/* Sidebar / mobile menu */}
